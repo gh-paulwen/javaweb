1d2736f58484ee4e1961392b672bf6f0c01f5329
paulWen <wen229267643@gmail.com>
2017-05-28 00:01:17 +0800

    add some front page

---
+++ src/main/webapp/front/js/variables.js
@@ -0,0 +1,15 @@
+var javaweb = {};
+
+javaweb.base = "/javaweb/";
+
+javaweb.AjaxParameter = function() {
+    this.dataType = "json";
+    this.type = "GET";
+    return this;
+};
+
+javaweb.createAP = function(url) {
+    var ap = new javaweb.AjaxParameter();
+    ap.url = javaweb.base + url;
+    return ap;
+};
